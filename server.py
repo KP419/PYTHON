10ffa8369c3537f32c7f208ea6edc1b2c5e25f5a
KP419 <koryplotts@yahoo.com>
2024-03-09 13:29:39 -0800

    API creations

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 from flask import Flask
 
+import json
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.get("/")
 def home():
     # code for home route
-    return "Hello, World!"
+    return "hello from flask"
+
+@app.get("/testing")
+def test():
+    return "hello from another page"
+
+@app.get("/about")
+def about():
+    me= {"name": "Kory Plotts"}
+    return json.dumps(me)
 
 app.run(debug=True)
